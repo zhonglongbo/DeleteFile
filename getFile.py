4c59206828062d815c63977eb1b8799152017f8e
midou <midou@mogu.com>
2019-09-08 15:36:51 +0800

    updae

--- getFile.py
+++ getFile.py
@@ -36,6 +36,6 @@ def rmFile(path):
     print(datetime.datetime.fromtimestamp(int(recent_time)).strftime("%Y-%m-%d %H:%M:%S"))
     for i in list:
         if os.path.getmtime(list[list.index(i)]) - recent_time > time:
-            print("result", list[list.index(i)], datetime.datetime.fromtimestamp(int(os.path.getmtime(list[list.index(i)])).strftime("%Y-%m-%d %H:%M:%S")))
+            print("result", list[list.index(i)], datetime.datetime.fromtimestamp(int(os.path.getmtime(list[list.index(i)]))).strftime("%Y-%m-%d %H:%M:%S"))
 if __name__ == '__main__':
     rmFile(path)
